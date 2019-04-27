a8fa17e2c955fd2f9377a9ddaaed32658a164b3f
Nadica08 <nadiabc3@gmail.com>
2019-04-27 10:22:43 -0700

    realized that I removed my bar graph code. Still not were it needs to be.

--- js/main.js
+++ js/main.js
@@ -1,45 +1,39 @@
 // Javascriptt file //
 console.log('This should be working');
 
-let data = []
-
-    fetch("./data/currencyrate.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log("Seems to be working!");
-        console.log(data.rates);
-        data = data.rates;
-        console.log(data);
-          for (data in dictionary) {
-            if (data.hasOwnProperty()) {
-                continue;
-        console.log(data, dictionary[data]);
-    }
+let state = {
+    "test": "data"
 };
 
-//    bar.style.height = height + "px";
-//    BarChart.appendChild(bar);
 
-  
-
-
- function getData() {
-  console.log('hello');
+ function doFetch() {
+  console.log('Begin Fetching JSON');
     fetch("./data/currencyrate.json")
     .then(response => response.json())
     .then(data => {
-        console.log("Seems to be working!");
-        console.log(data.rates);
-        setState({data:data});
-        console.log('test', data[0])
-        let BarChart = document.querySelector('#currency');
-        console.log('Accessing the Bar-chart')
+        console.log("Seems to be fetching currency-data");
         console.log(data);
-        let height = 70;
-        let bar = document.createElement("div");
-        bar.style.height = height + "px";
-        BarChart.appendChild(bar);;
-        });
-      }
+//            setState({data: data.rates});
+            console.log('test', data[0])
 
-BarChart();
+                for (let [key, value] of Object.entries(data.rates)) {
+                    console.log('key:', key, 'and the value:', value);
+//      for (let [data, value] of Object.entries(data.rates)) {
+//        console.log('test:', test, 'currency rates:', data.rates);
+        let Chart = document.querySelector("#currency");
+        console.log("Accessing the Bar-chart")
+        console.log(data);
+        let height = 80 / value;
+        let bar = document.createElement("div");
+        console.log("Attempting to retreive graph")
+        bar.classList.add("BarChart");
+        bar.style.height = height + "px";
+        console.log("Setting height of bar")
+        bar.textContent = key;
+        chart.appendChild(bar);
+      }
+//      setState.data = data;
+    });
+  }
+
+doFetch();
